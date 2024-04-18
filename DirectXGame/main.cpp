390920a7f4ee058362380c3a62e80eee5ffee50b
ryuto-alt <k023g0078@g.neec.ac.jp>
2024-04-18 12:09:29 +0900

    名前変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成  //test
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2C_05_ウノ_リュウト");
+	win->CreateGameWindow(L"LE2C_05_ウノ_リュウト_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
